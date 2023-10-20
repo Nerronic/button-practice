a00320b7dcc301b1ff40415fe4851b90846c1229
Ty <tyrellcarlton@gmail.com>
2023-10-20 09:06:23 -0700

    changed the navbar, added display:box

--- src/App.js
+++ src/App.js
@@ -1,67 +1,51 @@
-import ParticlesBg from "particles-bg"
+import ParticlesBg from "particles-bg";
 import React from 'react';
 import './App.css';
+import NavBar from "./Components/NavBar/Navbar";
 import Headshot from "./headshot/headshot.jpg";
 
-
-import NavBar from "./Components/NavBar/Navbar";
-
-
-
-
-
-
-// import Header from './Components/Header/Header';
-
-//BUTTON
-function MyButton(){
-  return(
-      <button> This works nicely</button>  
-  )
+// User
+const user = {
+  name: "Ty Carlton",
+  imageSize: 300,
+};
+//Profile Image
+const userImage =(
+  <img
+  className="headshot"
+  src={Headshot}
+  alt={'Photo of ' + user.name}
+  style={{
+    width: user.imageSize,
+    height: user.imageSize,
+    borderRadius: '50%',
+  }}
+/>
+)
+// BUTTON
+function MyButton() {
+  return (
+    <button>This works nicely</button>
+  );
 }
-//User
-const user ={
- name: "Ty Carlton",
-  imageSize:300,
-  
-}
 
-
-
-
-
-//LOGIN just leaving this here for my notes
-
-
-
-//renders the application
+// renders the application
 export default function App() {
   return (
     <div className="content">
-      
       <ParticlesBg type="cobweb" color="purple" bg={true} />
       <h1>{user.name}</h1>
-      <img
-        className="headshot"
-        src={Headshot}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-          borderRadius: '50%',
-          
-        }}
-      />
-    <NavBar/>
-      
-      
+      {userImage}
+      <NavBar />
       <MyButton />
       <p>
         Welcome to my new website.
         <div>
-          I have been learning how to program for the past few months now and I am excited to continue my journey.
+          I have been learning how to program for the past few months now, and I am excited to continue my journey.
         </div>
       </p>
+     
     </div>
-  )
-      }
+    
+  );
+}
--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,45 +1,39 @@
-import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavBar() {
-  const [navbarExpanded, setNavbarExpanded] = useState(false);
-
-  const handleNavbarToggle = () => {
-    setNavbarExpanded(!navbarExpanded);
-  };
-
+function Navigation() {
   return (
-    <Navbar
-      expand="lg"
-      className="bg-body-tertiary"
-      expanded={navbarExpanded}
-      onToggle={handleNavbarToggle}
-    >
-  <Container>
-    <Navbar.Brand href="#home">Learning all about React!!</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
+    <Navbar expand="lg" className="bg-body-tertiary" fixed="top" >
+      
+      <Container>
+        <Navbar.Brand href="#home">Home</Navbar.Brand>
         <br/>
-        <Nav.Link href="#link">Projects</Nav.Link>
-        <NavDropdown title="See More" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">What I've Got Cooking</NavDropdown.Item>
-          <br/>
-          <NavDropdown.Item href="#action/3.2">Videos!</NavDropdown.Item>
-          <br/>
-          <NavDropdown.Item href="#action/3.3">Coming Soon</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/34">Contact Me</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-);
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <br/>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <br/>
+            <NavDropdown title="More" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                
+              </NavDropdown.Item>
+              <br/>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default NavBar;
+export default Navigation;
